9cc378571d80a85d2e9915c0a4c54e8e89ac1f77
tanu2719 <86526469+tanu2719@users.noreply.github.com>
2024-07-09 21:17:20 +0530

    Create triggers.groovy

---
+++ triggers.groovy
@@ -0,0 +1,13 @@
+pipleline{
+  agent any
+  triggers{
+    pollSCM('17 21 09 * * *)
+  }
+  stages{
+    stage('Testing-Trigger'){
+      steps{
+      echo "tetsing trigger via pollSCM"
+    }
+    }
+  }
+}
